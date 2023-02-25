1c6fc1e22407afd6259731a088cbb4b7836540aa
gary-stroup-developer <stroup.uxuidesign@gmail.com>
2023-02-25 13:45:28 -0800

    initial commit. start layout setup

---
+++ javascript/navbar.js
@@ -0,0 +1,9 @@
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    x.className += " responsive";
+  }
+
+  function closeNav() {
+    var x = document.getElementById("myTopnav");
+    x.className = "topnav";
+  }
