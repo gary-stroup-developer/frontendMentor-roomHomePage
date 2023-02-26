cccb378e2b433b1201c052c22d2d2dbdc519b0d3
gary-stroup-developer <stroup.uxuidesign@gmail.com>
2023-02-25 18:05:20 -0800

    changed media query to min width and grid to flex

--- javascript/navbar.js
+++ javascript/navbar.js
@@ -1,9 +1,11 @@
 function myFunction() {
     var x = document.getElementById("myTopnav");
     x.className += " responsive";
+    document.getElementById("test").style.display = "none";
   }
 
   function closeNav() {
     var x = document.getElementById("myTopnav");
     x.className = "topnav";
+    document.getElementById("test").style.display = "block";
   }
